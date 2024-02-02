c0c586e838d1322befb41daea89888b460e3b706
randy-belbin <158519383+randy-belbin@users.noreply.github.com>
2024-02-01 20:44:57 -0500

    Update run.ps1
    Removed the replace function in favor of URL Encode

--- DataConnectors/RSA SecurID/AzFunSecurID/SecurIDTimerTrigger/run.ps1
+++ DataConnectors/RSA SecurID/AzFunSecurID/SecurIDTimerTrigger/run.ps1
@@ -268,10 +268,8 @@ function Update-CheckpointTime($CheckpointFile, $LastSuccessfulTime) {
 function Get-RSASecurIDEvent {
     $RSAKeyJson = Read-PrivateCerti
     $EventStartTime = Get-StartTime -CheckpointFile $CheckPointFile
-    $character = "\+"
-    if ($EventStartTime -match $character) {
-        $EventStartTime = $EventStartTime.replace('+', '%2B')
-    }    
+	$EventStartTime = [System.Web.HttpUtility]::UrlEncode($EventStartTime)
+ 
     # Format Endpoint Admin/User
     If ($RSA_Log_Type.ToLower() -eq "admin") {
         $RSA_API_End_Point = "$($RSAKeyJson.adminRestApiUrl)v1/adminlog/exportlogs?startTimeAfter=$($EventStartTime)"  
